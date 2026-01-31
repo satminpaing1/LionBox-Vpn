17bcb8b7754ac22c642b9b616e87cf252018954c
satminpaing1 <big680415@gmail.com>
2026-01-31 16:38:42 +0630

    Update assets.sh

--- buildScript/lib/assets.sh
+++ buildScript/lib/assets.sh
@@ -7,18 +7,21 @@ rm -rf $DIR
 mkdir -p $DIR
 cd $DIR
 
-# API ကို လှမ်းမမေးတော့ဘဲ Version နံပါတ်ကို အောက်မှာ တိုက်ရိုက် ရိုက်ထည့်လိုက်ပါပြီ
-# ----------------------------------------------------------------------
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
 
-#### GEOIP
-VERSION_GEOIP="202409120056" # Fixed version
+####
+VERSION_GEOIP=`get_latest_release "SagerNet/sing-geoip"`
 echo VERSION_GEOIP=$VERSION_GEOIP
 echo -n $VERSION_GEOIP > geoip.version.txt
 curl -fLSsO https://github.com/SagerNet/sing-geoip/releases/download/$VERSION_GEOIP/geoip.db
 xz -9 geoip.db
 
-#### GEOSITE
-VERSION_GEOSITE="202409120057" # Fixed version
+####
+VERSION_GEOSITE=`get_latest_release "SagerNet/sing-geosite"`
 echo VERSION_GEOSITE=$VERSION_GEOSITE
 echo -n $VERSION_GEOSITE > geosite.version.txt
 curl -fLSsO https://github.com/SagerNet/sing-geosite/releases/download/$VERSION_GEOSITE/geosite.db
